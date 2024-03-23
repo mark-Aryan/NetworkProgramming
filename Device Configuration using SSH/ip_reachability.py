02792e797bbc7eab51ec37e3816240dd6d2d86d4
mark-Aryan <akj4073@gmail.com>
2024-03-23 17:26:07 +0530

    Critical Update

--- Device Configuration using SSH/ip_reachability.py
+++ Device Configuration using SSH/ip_reachability.py
@@ -4,11 +4,10 @@ import subprocess
 
 # Checking IP Reachability
 def ip_reach(ip_list):
-
     for ip in ip_list:
         ip = ip.rstrip("\n")
 
-        ping_reply = subprocess.call('ping %s /n 2' % (ip), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        ping_reply = subprocess.call('ping %s -n 2' % ip, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         if ping_reply:
             print(f"{ip} is reachable: ")
@@ -16,6 +15,4 @@ def ip_reach(ip_list):
         else:
             print(f"XX IP {ip} not reachable, check connectivity and try again.")
             sys.exit()
-
-
 
---
+++ Device Configuration using SSH/thread.py
@@ -0,0 +1,22 @@
+"""
+Threading in Python is a technique that allows multiple tasks to run concurrently within a single process.
+This can be useful for improving the performance of your program,
+as it allows multiple tasks to be executed simultaneously,
+rather than having to wait for one task to finish before starting another.
+
+When you are purchasing a laptop or PC, you found 'Multi-Threading', etc. Our Processor also have threading to perform,
+several task simultaneously.
+"""
+
+import threading
+# Creating threads
+def create_threads(list, function):
+    threads = []
+
+    for ip in list:
+        th = threading.Thread(target=function, args=(ip,))  # args is a tuple with a single element
+        th.start()
+        threads.append(th)
+
+    for th in threads:
+        th.join()
